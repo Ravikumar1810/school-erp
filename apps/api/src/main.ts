578186a63504e24321bf75f8cb98d4ff3852c334
Ravikumar1810 <nkravikumar18@gmail.com>
2026-07-23 08:16:10 +0530

    fix: configure production frontend CORS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,19 +1,18 @@
 import { ValidationPipe } from "@nestjs/common";
-
 import { NestFactory } from "@nestjs/core";
-
+import { ConfigService } from "@nestjs/config";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule
-  );
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   app.setGlobalPrefix("api");
 
-  app.enableCors({
-    origin: "http://localhost:3000",
+  const frontendUrl = configService.get<string>("FRONTEND_URL");
 
+  app.enableCors({
+    origin: frontendUrl,
     credentials: true,
   });
 
